3cfba24671d9020b0d5b47a851aecb071ecb1901
abdul rehan <abdul.r@blubik.com>
2023-02-21 15:43:08 +0530

    change app.listen 3000 to 5000 and also change in console.lof=g 3000 to 5000)

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ app.post("/" , function(req, res){
 
 
 
-  app.listen(3000,function(){
-    console.log("working in 3000");
+  app.listen(5000,function(){
+    console.log("working in 5000");
   })
